0905d3a4308abc1b7bca77cdd3b9b27837fb750f
Samantha Henry <henrysamamntha.pro@gmail.com>
2026-05-05 15:12:06 -0400

    Application creee, fichier index.html cree, dossiers resources contenant les scripts et les styles ajoute, class Bb.java cree, ajout dependance PrimeFaces et ajout de index.xhtml dans web.xml

---
+++ src/main/java/ht/mbds/samanthahenry/tp1samanthahenry09/HelloServlet.java
@@ -0,0 +1,28 @@
+package ht.mbds.samanthahenry.tp1samanthahenry09;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "TP1 de Samantha Henry";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
